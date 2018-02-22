e47fab9eabc8510d709513339e81a965e7ed39e1
jenyu7 <jyu7@stuy.edu>
2018-02-21 22:21:08 -0500

    added draw fxn header

--- draw.h
+++ draw.h
@@ -10,5 +10,6 @@ void add_edge( struct matrix * points,
 	       double x1, double y1, double z1);
 void draw_lines( struct matrix * points, screen s, color c);
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c);
+void draw(int o, int x, int y, int x1, int y1, screen s, color c);
 
 #endif
